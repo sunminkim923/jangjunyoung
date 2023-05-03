169a1fe2f556bd29755d91559d08ade892845c1e
Quokka <sm.kim@last30min.com>
2023-05-03 13:04:47 +0900

    메인페이지 시안 완성

--- jangjunyoung/src/pages/index.js
+++ jangjunyoung/src/pages/index.js
@@ -98,7 +98,17 @@ export default function Home() {
           </div>
         </div>
       </div>
-      <div className={"h-[320px] w-screen opacity-80"}>
+      <div className={"h-[320px] w-screen opacity-100"}>
+        <div
+          className={
+            "h-[320px] w-screen absolute z-20 text-[white] flex justify-center items-center text-[40px] font-[200]"
+          }
+        >
+          30년 전통의 노하우로 최고의 맛을위해 매일 노력합니다
+        </div>
+        <div
+          className={"h-[320px] w-screen bg-[black] absolute z-10 opacity-50"}
+        />
         <Image
           className={"object-cover"}
           src={"images/main_img_01_pc.svg"}
@@ -110,6 +120,28 @@ export default function Home() {
             height: "320px",
           }}
         />
+      </div>
+      <div className={"h-[720px] flex items-center justify-evenly"}>
+        <div>
+          <div className={"text-[32px] font-[600]"}>오시는길</div>
+          <div className={"text-[16px] font-[200] pt-5"}>
+            <div>자랑처럼 새워 별 봅니다. 마리아 없이 새겨지는 봅니다.</div>
+            <div>경, 슬퍼하는 가득 버리었습니다.</div>
+            <div>않은 이런 별에도 속의 못 덮어 걱정도 있습니다.</div>
+          </div>
+        </div>
+        <div
+          className={
+            "w-[360px] h-[200px] bg-[lightgray] flex items-center justify-center"
+          }
+        >
+          IMG
+        </div>
+      </div>
+      <div
+        className={"h-[200px] bg-[#cccccc] flex items-center justify-center"}
+      >
+        Footer. 들어갈 정보. (주소, 사업자번호, 대표자, 연락처 등)
       </div>
     </>
   );
