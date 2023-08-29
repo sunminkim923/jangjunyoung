670f62ad9da89c9b6776abc190241071be22bb0c
Quokka <sm.kim@last30min.com>
2023-08-29 15:16:40 +0900

    title 추가

--- jangjunyoung/src/pages/_document.js
+++ jangjunyoung/src/pages/_document.js
@@ -1,13 +1,15 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <title>장준영 닭강정</title>
+      </Head>
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
